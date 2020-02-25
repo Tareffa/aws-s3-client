5a17b715fa0d210e3dcfe4d052c280ca4caf92a7
Lucas Martins <dev.lucasmartins@gmail.com>
2020-02-25 16:15:09 -0300

    criado método para buscar objeto do s3

--- src/main/java/br/com/tareffa/awss3client/services/AwsService.java
+++ src/main/java/br/com/tareffa/awss3client/services/AwsService.java
@@ -11,8 +11,10 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
 import org.springframework.stereotype.Service;
@@ -22,23 +24,24 @@ import br.com.tareffa.awss3client.domain.models.Bucket;
 @Service
 public class AwsService {
 
-    public void upload(File file, String filename, Bucket bucket) {
-        this.upload(file, filename, bucket, s3client(bucket));
-    }
+	@Deprecated
+	public void upload(File file, String key, Bucket bucket) {
+		this.upload(key, file, bucket);
+	}
 
-    public void upload(File file, String filename, Bucket bucket, AmazonS3 s3client) {
-		
+    public void upload(String key, File file, Bucket bucket) {
+		AmazonS3 s3client = s3client(bucket);
+
 		try {
-			PutObjectResult result = s3client.putObject(new PutObjectRequest(bucket.getBucketName(), filename, file));
-			
-			ObjectMetadata meta = result.getMetadata();
-			
-			for (Map.Entry<String, Object> entry : meta.getRawMetadata().entrySet()) {
-				System.out.println(entry.getKey() + " - " + entry.getValue());
-			}
+			// envia o objeto para o s3.
+			putObject(key, file, bucket, s3client);
 
-	        System.out.println("===================== Upload File - Done! =====================");
-	        
+			// busca o objeto do s3
+			S3Object object = getObject(key, bucket, s3client);
+
+			System.out.println("Object Key  ................. " + object.getKey());
+			System.out.println("Object Redirect Location  ... " + object.getRedirectLocation());
+
 		} catch (AmazonServiceException ase) {
 			System.out.println("Caught an AmazonServiceException from PUT requests, rejected reasons:");
 			System.out.println("Error Message:    " + ase.getMessage());
@@ -50,6 +53,37 @@ public class AwsService {
             System.out.println("Caught an AmazonClientException: ");
             System.out.println("Error Message: " + ace.getMessage());
         }
+
+    }
+
+	public void upload(File file, String filename, Bucket bucket, AmazonS3 s3client) {
+		
+		try {
+			PutObjectResult result = s3client.putObject(new PutObjectRequest(bucket.getBucketName(), filename, file));
+			
+	        System.out.println("===================== Upload File - Done! =====================");
+
+		} catch (AmazonServiceException ase) {
+			System.out.println("Caught an AmazonServiceException from PUT requests, rejected reasons:");
+			System.out.println("Error Message:    " + ase.getMessage());
+			System.out.println("HTTP Status Code: " + ase.getStatusCode());
+			System.out.println("AWS Error Code:   " + ase.getErrorCode());
+			System.out.println("Error Type:       " + ase.getErrorType());
+			System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException: ");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+	}
+
+
+	private S3Object getObject(String key, Bucket bucket, AmazonS3 s3client) {
+		return s3client.getObject(new GetObjectRequest(bucket.getBucketName(), key));
+	}
+
+	private PutObjectResult putObject(String key, File file, Bucket bucket, AmazonS3 s3client) 
+			throws AmazonServiceException, AmazonClientException {
+		return s3client.putObject(new PutObjectRequest(bucket.getBucketName(), key, file));
 	}
 
     private AmazonS3 s3client(Bucket bucket) {
